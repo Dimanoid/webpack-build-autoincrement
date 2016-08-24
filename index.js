b1f3f437aacaa3f84fd2a03aa59e83bfa787cbed
Dmitry Mukhin <dimanomail@gmail.com>
2016-08-24 12:09:35 +0300

    watch only updates original file, output stays unchanged to avoid cyclic recompile

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-        
+
 var BuildAutoInc = function (file, config) {
     this.file = file;
     this.config = config || {};
@@ -13,12 +13,12 @@ BuildAutoInc.prototype.read = function (file, callback) {
             callback(ver);
         }
         else {
-        var v = buf.toString().replace(/\n/g, '').split('.');
-        ver.major = v[0];
-        ver.minor = v[1];
-        ver.patch = v[2];
-        ver.build = v[3];
-        callback(ver);
+            var v = buf.toString().replace(/\n/g, '').split('.');
+            ver.major = v[0];
+            ver.minor = v[1];
+            ver.patch = v[2];
+            ver.build = v[3];
+            callback(ver);
         }
     });
 };
@@ -54,18 +54,30 @@ BuildAutoInc.prototype.write = function (cout, ver, callback) {
 
 BuildAutoInc.prototype.apply = function (compiler) {
     var self = this;
+
     compiler.plugin('run', function (compilation, callback) {
         self.read(self.file, function (ver) {
-            ver.build++;
-            console.log('--------', ver);
+            ver.patch++;
+            console.log('----------------------', ver);
             ver.text = ver.major + '.' + ver.minor + '.' + ver.patch + '.' + ver.build;
             if (self.config && self.config.output) {
                 for (var i = 0; i < self.config.output.length; i++) {
                     self.write(self.config.output[i], ver, null);
                 }
             }
-            self.write({type:'text', file: self.file}, ver, callback);
+            self.write({ type: 'text', file: self.file }, ver, callback);
         });
     });
+
+    compiler.plugin('watch-run', function (compilation, callback) {
+        self.read(self.file, function (ver) {
+            ver.build++;
+            console.log('----------------------', ver);
+            ver.text = ver.major + '.' + ver.minor + '.' + ver.patch + '.' + ver.build;
+            self.write({ type: 'text', file: self.file }, ver, callback);
+        });
+    });
+
 };
+
 module.exports = BuildAutoInc;
